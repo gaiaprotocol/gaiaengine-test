254e6af6b63c70ceb635a89d9626968f9c1fbf19
yjgaia <yjdev88@gmail.com>
2024-09-14 14:13:58 +0900

    Update Collision Test

--- game/collision-test/CollisionTest.ts
+++ game/collision-test/CollisionTest.ts
@@ -1,26 +1,55 @@
 import { BodyNode, View } from "@common-module/app";
 import {
-  CollisionChecker,
+  Collidable,
+  Collider,
+  ColliderType,
+  CollisionDetector,
+  EllipseNode,
   FPSDisplay,
   Fullscreen,
-  GameObject,
   Interval,
+  Movable,
   Sprite,
 } from "@gaiaengine/2d";
 import { AnalogJoystick } from "@gaiaengine/2d-joystick";
-import { IntegerUtil } from "../../../ts-module/lib/index.js";
+import { IntegerUtils } from "../../../ts-module/lib/index.js";
 
-class Hero extends GameObject {
+class Hero extends Movable implements Collidable {
+  public colliders: Collider[] = [{
+    type: ColliderType.Ellipse,
+    x: 0,
+    y: 0,
+    width: 64,
+    height: 64,
+  }];
+
   constructor(x: number, y: number) {
     super(x, y);
     this.append(new Sprite(0, 0, "/assets/cat.png"));
+
+    this.append(
+      new EllipseNode(0, 0, 64, 64, undefined, { width: 2, color: 0xff0000 }),
+    );
   }
 }
 
-class Bullet extends GameObject {
+class Bullet extends Movable implements Collidable {
+  public colliders: Collider[] = [{
+    type: ColliderType.Ellipse,
+    x: 0,
+    y: 0,
+    width: 40,
+    height: 40,
+  }];
+
   constructor(x: number, y: number) {
     super(x, y);
     this.append(new Sprite(0, 0, "/assets/bullet.png"));
+    this.speedX = -500;
+
+    this.append(
+      new EllipseNode(0, 0, 40, 40, undefined, { width: 2, color: 0xff0000 }),
+    );
   }
 }
 
@@ -33,18 +62,24 @@ export default class CollisionTest extends View<Fullscreen> {
     super();
     this.container = new Fullscreen().appendTo(BodyNode);
 
-    this.heros = Array.from({ length: 10 }, () =>
+    /*this.heros = Array.from({ length: 10 }, () =>
       new Hero(
-        IntegerUtil.random(0, this.container.width),
-        IntegerUtil.random(0, this.container.height),
-      ));
+        IntegerUtils.random(
+          -this.container.width / 2,
+          this.container.width / 2,
+        ),
+        IntegerUtils.random(
+          -this.container.height / 2,
+          this.container.height / 2,
+        ),
+      ));*/
 
-    this.hero = new Hero(this.container.width / 2, this.container.height / 2);
+    this.hero = new Hero(0, 0);
     this.heros.push(this.hero);
 
     this.container.root.append(
       ...this.heros,
-      new Interval(100, () => this.createBullet()),
+      new Interval(1, () => this.createBullet()),
       new AnalogJoystick(
         (angle) => {
           console.log("onMove", angle);
@@ -53,8 +88,9 @@ export default class CollisionTest extends View<Fullscreen> {
           console.log("onRelease");
         },
       ),
-      new CollisionChecker(this.heros, this.bullets, (hero) => {
+      new CollisionDetector(this.heros, this.bullets, (hero, bullet) => {
         if (hero === this.hero) {
+          console.log(hero.absoluteTransform, bullet.absoluteTransform);
           console.log("hero hit");
         } else {
           console.log("dummy hero hit");
@@ -66,8 +102,8 @@ export default class CollisionTest extends View<Fullscreen> {
 
   private createBullet() {
     const bullet = new Bullet(
-      IntegerUtil.random(0, this.container.width),
-      IntegerUtil.random(0, this.container.height),
+      this.container.width / 2,
+      IntegerUtils.random(-this.container.height, this.container.height / 2),
     );
     this.bullets.push(bullet);
     this.container.root.append(bullet);
